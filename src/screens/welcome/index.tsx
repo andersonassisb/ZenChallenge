b42bf4efe840114b8c478028f99f04b666f5f45c
Locação <locacao@C02Y80UFJG5J.local>
2022-06-07 15:16:16 -0300

    refactor(Welcome): added animations

--- src/screens/welcome/index.tsx
+++ src/screens/welcome/index.tsx
@@ -1,22 +1,73 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { Text, SafeAreaView, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import type { StackNavigationProp } from '@react-navigation/stack'
-import LottieView from 'lottie-react-native'
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  interpolate,
+  withTiming,
+  withSpring,
+  Easing,
+} from 'react-native-reanimated'
+import Logo from '../../assets/images/logo.png'
 import Button from '../../components/Button'
 import styles from './styles'
 
 const Welcome: React.FC = () => {
   const navigation = useNavigation<StackNavigationProp<any>>()
+  const rotation = useSharedValue(0)
+  const scale = useSharedValue(1)
+
+  React.useEffect(() => {
+    if (scale) {
+      scale.value = withTiming(0.5, {
+        duration: 3000,
+        easing: Easing.out(Easing.exp),
+      })
+      setTimeout(() => {
+        scale.value = withTiming(1, {
+          duration: 3000,
+          easing: Easing.out(Easing.exp),
+        })
+      }, 3000);
+    }
+  }, [scale])
+
+  React.useEffect(() => {
+    if (rotation) {
+      rotation.value = withTiming(360, {
+        duration: 3000,
+        easing: Easing.out(Easing.exp),
+      })
+    }
+  }, [rotation])
+
+  const animatedViewStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: withSpring(scale.value),
+        },
+      ],
+    }
+  })
+
+  const animatedImageStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotateZ: `${rotation.value}deg`,
+        },
+      ],
+    }
+  })
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.animation}>
-        <LottieView
-          source={require('../../assets/welcome.json')}
-          loop
-          autoPlay
-        />
-      </View>
+      <Animated.View style={animatedViewStyles}>
+        <Animated.Image source={Logo} style={animatedImageStyles} />
+      </Animated.View>
       <Text style={styles.title}>Welcome to the ZenChallenge!</Text>
       <Button
         onPress={() => navigation.navigate('Main')}
